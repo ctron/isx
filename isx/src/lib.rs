86a90e5b6a72507f92561723cbc7e510f86987f7
Jens Reimann <ctron@dentrassi.de>
2025-07-23 16:35:31 +0200

    docs: add an introduction to the crate

--- isx/src/lib.rs
+++ isx/src/lib.rs
@@ -1,3 +1,10 @@
+//! Provide some traits (and implementations) for types in the spirit of `is_*` (like `is_empty`)
+//!
+//! The idea is to provide standard traits for types to e.g. check "if something is empty"? In
+//! some cases, one might just be interested if something is empty. However, Rust currently doesn't
+//! offer a trait like `IsEmpty`, although a lot of types provide a `is_empty(&self) -> bool`
+//! function. This crate tries to fix that.
+
 #![cfg_attr(not(feature = "std"), no_std)]
 
 #[cfg(feature = "alloc")]
